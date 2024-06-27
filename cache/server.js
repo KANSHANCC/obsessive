8acc522c1f71d8fd69367f5d04c6e5d016565640
cc <haohaohao0128@outlook.com>
2024-06-27 21:24:02 +0800

    f

--- cache/server.js
+++ cache/server.js
@@ -9,28 +9,43 @@ http.createServer((req, res) => {
         res.end(data)
     } else if (u.pathname === '/resource/images/1.jpg') {
         let data = fs.readFileSync('./resource/images/1.jpg')
-        // res.setHeader('Expires', new Date('2024-06-27').toUTCString())
-        res.setHeader('Cache-Control', 'max-age=31536000')
+        res.setHeader('Expires', new Date('2024-06-27').toUTCString())
+        res.setHeader('Cache-Control', 'max-age=5')
         res.end(data)
     } else if (u.pathname === '/resource/images/2.jpg') {
-        let hashHistory = req.headers['if-none-match']
         let data = fs.readFileSync('./resource/images/2.jpg')
-        let ETagValue = getEtag(data)
-        res.setHeader('ETag', ETagValue)
-        if (hashHistory !== ETagValue) {
-            res.writeHead(200)
-            res.end(data)
-        } else {
-            res.writeHead(304)
+        let eTag = getEtag(data)
+        if (req.headers['if-none-match'] === eTag) {
+            res.statusCode = 304
             res.end()
+        } else {
+            res.setHeader('ETag', eTag)
+            res.statusCode = 200
+            res.end(data)
         }
+    } else if (u.pathname === '/resource/images/3.jpg') {
+        let stat = fs.statSync('./resource/images/3.jpg')
+        let lastModified = stat.mtime.toUTCString()
+        res.setHeader('Cache-Control', 'no-cache')
+        if (req.headers['if-modified-since'] === lastModified) {
+            res.statusCode = 304
+            res.end()
+        } else {
+            let data = fs.readFileSync('./resource/images/3.jpg')
+            res.statusCode = 200
+            res.setHeader('Last-Modified', lastModified)
+            res.end(data)
+        }
+    } else {
+        res.statusCode = 404
+        res.end()
     }
 }).listen(3000, () => {
     console.log('http server started on port 3000');
 })
 
 
-function getEtag (data) {
+function getEtag(data) {
     let hash = crypto.createHash('md5')
     hash.update(data)
     return hash.digest('hex')
